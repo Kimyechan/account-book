24722053a9f01d1179e1c8714d2de1e6ebc3700e
Kimyechan <vlvkcjswo7@naver.com>
2020-10-14 17:47:58 +0900

    Feat: DB Connection
    use JDBC

--- src/com/company/accountbook/Main.java
+++ src/com/company/accountbook/Main.java
@@ -1,8 +1,9 @@
 package com.company.accountbook;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
-    public static void main(String[] args) {
-
-    }
 }
