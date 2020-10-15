50d52b5bb145a2bda6017ec9f8587c6318ce418f
Tezla-Lee <tezla.lee@gmail.com>
2020-10-15 17:22:34 +0900

    checkout for pull

--- src/com/company/accountbook/Main.java
+++ src/com/company/accountbook/Main.java
@@ -1,8 +1,11 @@
 package com.company.accountbook;
 
+import com.company.accountbook.dao.ReportDAO;
+import com.company.accountbook.dto.AccountBook;
 import com.company.accountbook.service.MonthCheckAccountBook;
 import com.company.accountbook.util.Menu;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
@@ -10,11 +13,12 @@ public class Main {
         MonthCheckAccountBook monthCheckAccountBook = new MonthCheckAccountBook();
         Scanner sc = new Scanner(System.in);
         Menu menu = Menu.getInstance();
+        new ReportDAO(new AccountBook("jb")).addReport("a", 13000, "", LocalDate.now());
 
         // 초기 화면 출력 (이번달 달력)
-        monthCheckAccountBook.printCalendar();
+//        monthCheckAccountBook.printCalendar();
 
         // 메인 메뉴 출력
-        menu.mainMenuPrint();
+//        menu.mainMenuPrint();
     }
 }
