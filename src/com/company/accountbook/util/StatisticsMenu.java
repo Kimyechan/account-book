c06f6b7f82f8cb0fc1d9ed05950b5b3298da823e
Kimyechan <vlvkcjswo7@naver.com>
2020-10-21 14:01:40 +0900

    Refactor: Separate DB Server info to file
    Add DBServerInfo File to .gitignore

--- src/com/company/accountbook/util/StatisticsMenu.java
+++ src/com/company/accountbook/util/StatisticsMenu.java
@@ -2,10 +2,14 @@ package com.company.accountbook.util;
 
 import com.company.accountbook.service.ReportService;
 
+import java.util.Map;
 import java.util.Scanner;
 
 public class StatisticsMenu {
     Scanner sc = new Scanner(System.in);
+    private int totalMoney;
+    Map<String, Integer> expense;
+    Map<String, Integer> income;
     InputDateMenu inputDateMenu = InputDateMenu.getInstance();
 
     ReportService reportService = new ReportService();
@@ -40,26 +44,88 @@ public class StatisticsMenu {
             System.out.println();
         }
 
-        if (number.equals("1")) { // 일별 통계
-            inputDateMenu.inputYearMonthDay();
-            reportService.showStatistics(reportService.getDayReports(inputDateMenu.year, inputDateMenu.month, inputDateMenu.day));
-            System.out.println();
-        } else if (number.equals("2")) { // 주별 통계
-            System.out.println("서비스 예정입니다.");
-            System.out.println();
-        } else if (number.equals("3")) { // 월별 통계
-            inputDateMenu.inputYearMonth();
-            reportService.showStatistics(reportService.getMonthReports(inputDateMenu.year, inputDateMenu.month));
-            System.out.println();
-        } else if (number.equals("4")) { // 연도별 통계
-            inputDateMenu.inputYear();
-            reportService.showStatistics(reportService.getYearReports(inputDateMenu.year));
-            System.out.println();
-        } else {
-            MainMenu.getInstance().mainMenuPrint();
-            System.out.println();
+        switch (number) {
+            case "1":  // 일별 통계
+                // 날짜 입력
+                inputDateMenu.inputYearMonthDay();
+
+                // 지출 리스트 가져옴
+                expense = reportService.getDayExpenseStatics(inputDateMenu.year, inputDateMenu.month, inputDateMenu.day);
+                // 수입 리스트 가져옴
+                income = reportService.getDayIncomeStatics(inputDateMenu.year, inputDateMenu.month, inputDateMenu.day);
+
+                // 지출 출력
+                printStatistics(false, expense);
+                System.out.println();
+                // 수입 출력
+                printStatistics(true, income);
+                System.out.println();
+                break;
+
+            case "2":  // 주별 통계
+                System.out.println("서비스 예정입니다.");
+                System.out.println();
+                break;
+
+            case "3":  // 월별 통계
+                inputDateMenu.inputYearMonth();
+                // 지출 리스트 가져옴
+                expense = reportService.getMonthExpenseStatics(inputDateMenu.year, inputDateMenu.month);
+                // 수입 리스트 가져옴
+                income = reportService.getMonthIncomeStatics(inputDateMenu.year, inputDateMenu.month);
+
+                // 지출 출력
+                printStatistics(false, expense);
+                System.out.println();
+                // 수입 출력
+                printStatistics(true, income);
+                System.out.println();
+                break;
+
+            case "4":  // 연도별 통계
+                inputDateMenu.inputYear();
+                // 지출 리스트 가져옴
+                expense = reportService.getYearExpenseStatics(inputDateMenu.year);
+                // 수입 리스트 가져옴
+                income = reportService.getYearIncomeStatics(inputDateMenu.year);
+
+                // 지출 출력
+                printStatistics(false, expense);
+                System.out.println();
+                // 수입 출력
+                printStatistics(true, income);
+                System.out.println();
+                break;
+
+            default:
+                MainMenu.getInstance().mainMenuPrint();
+                System.out.println();
+                break;
         }
         StatisticsMenu.getInstance().statisticsMenuPrint();
         System.out.println();
+    }
+
+
+    public void printStatistics(boolean isIncome,Map<String, Integer> reportList) {
+        totalMoney = 0;
+
+        if(isIncome) {
+            System.out.println("지출");
+        } else {
+            System.out.println("수입");
+        }
+        if (reportList.isEmpty()) {
+            System.out.println("- 내역 없음");
+        } else {
+            for (Map.Entry<String, Integer> entry : reportList.entrySet()) {
+                totalMoney += entry.getValue();
+            }
+            for (Map.Entry<String, Integer> entry : reportList.entrySet()) {
+                System.out.println("- " + entry.getKey() + ": " + entry.getValue() + " (" + ((float) entry.getValue() * 100 / (float) totalMoney) + " %)");
+            }
+            System.out.println();
+            System.out.println("총 금액: " + totalMoney);
+        }
     }
 }
--- src/com/company/accountbook/vo/ExpenseCategory.java
+++ src/com/company/accountbook/vo/ExpenseCategory.java
@@ -1,5 +1,5 @@
 package com.company.accountbook.vo;
 
-public enum ExpenseCategory {
+public enum ExpenseCategory{
     FOOD, CLOTH, TAX
 }
