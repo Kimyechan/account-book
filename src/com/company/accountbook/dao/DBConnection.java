05ee020d3a3855c1fa42f96f07b8fee352474a09
Tezla-Lee <tezla.lee@gmail.com>
2020-10-21 12:56:39 +0900

    Refact: Separate ReportService and StatisticsMenu

--- src/com/company/accountbook/dao/DBConnection.java
+++ src/com/company/accountbook/dao/DBConnection.java
@@ -5,10 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String server = "localhost"; // MySQL 서버 주소
+    private static final String server = "52.79.38.134"; // MySQL 서버 주소
     private static final String database = "accountbook"; // MySQL DATABASE 이름
     private static final String user_name = "root"; //  MySQL 서버 아이디
-    private static final String password = "12341234"; // MySQL 서버 비밀번호
+    private static final String password = "jbs"; // MySQL 서버 비밀번호
 
     public static Connection getConnection() {
         Connection con = null;
